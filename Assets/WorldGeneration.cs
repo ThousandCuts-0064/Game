c7f0468dadfd5694b934ea2e77b0ac1b5e9090ee
ThousandCuts-0064 <92256555+ThousandCuts-0064@users.noreply.github.com>
2022-07-15 14:20:40 +0300

    .

--- Assets/WorldGeneration.cs
+++ Assets/WorldGeneration.cs
@@ -4,32 +4,50 @@ using UnityEngine;
 
 public class WorldGeneration : MonoBehaviour
 {
-    private static readonly Vector3[] _verticies;
-    private static readonly Vector3[] _verticiesReversed;
-    private static readonly int[] _triangles = { 0, 1, 2 };
-    private Mesh _mesh;
+    private const float SQRT_3 = 1.732050807568877293527446341505872366942805253810380628055806f;
+    private const float SIDE = 1;
+    private const float HALF_SIDE = SIDE / 2;
+    private const float R_IN = SIDE * SQRT_3 / 6;
+    private const float R_CIRCUM = SIDE * SQRT_3 / 3;
+    private const float H = SIDE * SQRT_3 / 2;
 
-    static WorldGeneration()
-    {
-        float sqrt3 = Mathf.Sqrt(3);
-        float Sqrt3Div3 = sqrt3 / 3;
-        float sqrt3Div6 = sqrt3 / 6;
-        _verticies = new Vector3[] { new(-0.5f, 0, -sqrt3Div6), new(0, 0, Sqrt3Div3), new(0.5f, 0, -sqrt3Div6) };
-        _verticiesReversed = new Vector3[] { new(0.5f, 0, sqrt3Div6), new(0, 0, -Sqrt3Div3), new(-0.5f, 0, sqrt3Div6) };
-    }
+    private const int SIDE_UNITS = 147;
+    private const float SIDE_LENGTH = SIDE * SIDE_UNITS;
+    private const float HALF_SIDE_LENGTH = SIDE_LENGTH / 2;
+    private const int VERTICIES_COUNT = 3 * SIDE_UNITS * (SIDE_UNITS + 1) + 1;
+    private const int TRIANGLES_TOTAL = 6 * SIDE_UNITS * SIDE_UNITS;
+
+    private static readonly Vector3[] _verticies = new Vector3[VERTICIES_COUNT];
+    private static readonly int[] _triangles = new int[TRIANGLES_TOTAL];
+    private static GameObject _gameObject;
+    [SerializeField] private Material _material;
 
     private void Awake()
     {
-        _mesh = new()
-        {
-            vertices = _verticies,
-            triangles = _triangles
-        };
+        //_gameObject = new();
+        //_gameObject.AddComponent<MeshFilter>().mesh = new()
+        //{
+        //    vertices = _verticies,
+        //    triangles = _triangles
+        //};
+        //_gameObject.AddComponent<MeshRenderer>();
+        //_gameObject.transform.parent = transform;
 
-        GameObject @object = new();
-        @object.transform.parent = transform;
-        var meshFilter = @object.AddComponent<MeshFilter>();
-        meshFilter.mesh = _mesh;
-        var meshRenderer = @object.AddComponent<MeshRenderer>();
+        int vertIndex = 0;
+        int offset = 1;
+        for (int row = 0; row < 2 * SIDE_UNITS + 1; row++)
+        {
+            for (int col = 0; col < SIDE_UNITS + offset; col++)
+                _verticies[vertIndex++] = new(-HALF_SIDE_LENGTH - offset * HALF_SIDE + col * SIDE, 0, -H * SIDE_UNITS + row * H);
+            offset += row < SIDE_UNITS ? 1 : -1;
+        }
+
+        for (int i = 0; i < VERTICIES_COUNT; i++)
+        {
+            var go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            go.transform.parent = transform;
+            go.transform.localPosition = _verticies[i];
+            //go.transform.localScale = new(0.1f, 0.1f, 0.1f);
+        }
     }
 }
